ac7b8c2c911e3fc0d8b7cb93d9e7b1af68d0c9c9
SirMartin <sirmartin@gmail.com>
2012-03-29 13:30:41 +0200

    Test Creado para los splits de identitys

---
+++ RiverSalaMVC.Tests/OtherTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RiverSalaMVC;
+using RiverSalaMVC.Controllers;
+
+namespace RiverSalaMVC.Tests.Controllers
+{
+    [TestClass]
+    public class OtherTests
+    {
+        [TestMethod]
+        public void SplitSeparator()
+        {
+            String identity = "sirmartin@gmail.com-*/*/-Eduardo-*/*/-True";
+
+            String[] arrayIdentity = identity.Split(Utils.Constantes.IdentitySeparator.ToCharArray());
+
+            String[] arrayIdentity2 = identity.Split(Utils.Constantes.IdentitySeparator.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+            
+        }
+    }
+}
