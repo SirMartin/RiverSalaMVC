0361e36fd1f4b6eaee6cf2c20f2292a14089a401
SirMartin <sirmartin@gmail.com>
2012-04-03 16:43:42 +0200

    Creacion de la lista de correo

---
+++ RiverSalaMVC/Controllers/ListaCorreoController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RiverSalaMVC.Models.Security;
+using RiverSalaMVC.Models;
+using RiverSalaMVC.Utils;
+
+namespace RiverSalaMVC.Controllers
+{
+    public class ListaCorreoController : BaseController
+    {
+        //
+        // GET: /ListaCorreo/
+
+        [Authorize]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Authorize]
+        public ActionResult Index(ListaCorreoModel listaCorreo)
+        {
+            //Cogemos el usuario autenticado.
+            String[] arrayIdentity = User.Identity.Name.Split(RiverSalaMVC.Utils.Constantes.IdentitySeparator.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+            string username = arrayIdentity[1];
+            listaCorreo.Autor = username;
+
+            //Ponemos los saltos de linea.
+            listaCorreo.Contenido = listaCorreo.Contenido.Replace("\r\n", "<br/>");
+
+            //Enviamos el correo.
+            MailService.SendEmailListaCorreo(listaCorreo);
+            
+            //Volvemos a la vista.
+            return RedirectToAction("Index", "Home");
+        }
+
+    }
+}
---
+++ RiverSalaMVC/Models/ListaCorreoModels.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace RiverSalaMVC.Models
+{
+    public class ListaCorreoModel
+    {
+        [Display(Name = "Autor")]
+        public String Autor { get; set; }
+
+        [Display(Name = "Titulo")]
+        public String Titulo { get; set; }
+
+        [Display(Name = "Contenido")]
+        public String Contenido { get; set; }
+    }
+}
